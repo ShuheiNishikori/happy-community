b4651a92b861e2ba97ed72c20fe07a188d837fbe
Adachi <tugaru124@gmail.com>
2017-12-13 02:49:48 +0000

    make homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 	has_many 		:messages
 
 	validates 		:name, 	presence: true,	length: {maximum: 50}
-	validates		:email, presence: true,	length: {maximum: 255}
+	validates		:email, presence: true,	length: {maximum: 255}, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
