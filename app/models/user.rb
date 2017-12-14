87eab1bea0ae4d61b02002eef470153e0956763f
WindUp345 <s153073@matsu.shimane-u.ac.jp>
2017-12-14 08:38:12 +0000

    ログインログアウト機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
 	has_many 		:event_members
 	has_many 		:messages
 
+	has_secure_password
+	validates	:password, presence: true, length: {minimum:6}
 	validates 		:name, 	presence: true,	length: {maximum: 50}
 	validates		:email, presence: true,	length: {maximum: 255}
+	
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engin.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'chat_messages/index'
-
+  #get 'sessions/new'
+  
+  #get 'chat_messages/index'
+  get '/index', to: 'chat_messages#index'
+  
   resources :communities
   resources :messages
   resources :community_tags
@@ -10,11 +13,17 @@ Rails.application.routes.draw do
   resources :events
   resources :skills
   resources :users
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+  get '/help',  to: 'static_pages#help'
+  #get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+  #get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
