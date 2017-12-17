89071048390564630db9d27b047f6fb5a69a06b3
masashi <matyamss_sl1009@icloud.com>
2017-12-17 06:21:52 +0000

    検索機能

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,5 +1,7 @@
 class Community < ApplicationRecord
 	has_many :events
 	has_many :members
-	has_many :c_tags
+	has_many :community_tags
+	has_many :tags, through: :community_tags
+	accepts_nested_attributes_for :tags
 end
